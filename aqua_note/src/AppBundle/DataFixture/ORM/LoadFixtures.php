611a3e6c65b05d4543da02b1720e59d313bf720f
Vitaliy <gapon007@ukr.net>
2019-03-22 16:08:29 +0200

    Use faker data

---
+++ aqua_note/src/AppBundle/DataFixture/ORM/LoadFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\DataFixture\ORM;
+
+
+use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\FixtureSet;
+use Nelmio\Alice\Loader\FakeDataLoader;
+use Nelmio\Alice\Loader\NativeLoader;
+
+
+class LoadFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $files = [
+            'genus' => __DIR__.'/fixters.yml',
+        ];
+
+        $loader    = new NativeLoader();
+        $objectSet = $loader->loadFiles($files);
+
+        foreach ($objectSet->getObjects() as $key => $object) {
+            $this->addReference($key, $object);
+            $manager->persist($object);
+        }
+
+        $manager->flush();
+    }
+
+
+}
