1b3cad52142dd9a8ccc76beb9f90caae26b63c19
Vitaliy <gapon007@ukr.net>
2019-03-24 13:12:11 +0200

    Create Simple Form and render info from form

--- aqua_note/src/AppBundle/Controller/Admin/GenusAdminController.php
+++ aqua_note/src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -12,6 +12,7 @@ namespace AppBundle\Controller\Admin;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Form\GenusFormType;
+use Symfony\Component\HttpFoundation\Request;
 
 class GenusAdminController extends Controller
 {
@@ -21,9 +22,16 @@ class GenusAdminController extends Controller
      * @Route("/genus/new", name="admin_genus_new")
      *
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
         $form = $this->createForm(GenusFormType::class);
+
+        //use only for POST
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            dump($form->getData()); die();
+        }
 
         return $this->render('admin/genus/new.html.twig', [
             'genusForm' => $form->createView()
